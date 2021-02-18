ba037fb1e3f729f68c75361cb29cd4709d38412b
Anthony Mota <anthony.mota5@yahoo.com>
2021-02-17 20:05:13 -0800

    mailing address dictionary update

--- main.py
+++ main.py
@@ -15,8 +15,17 @@ document = Document('data_document.docx')
 for k in document.paragraphs:
     for j in range(len(k.text)):
         if k.text[j]==':':
-            variables[k.text[:j]]=k.text[j+2:]
+            if k.text[j+1]==" ":
+                variables[k.text[:j]]=k.text[j+2:]
+            else:
+                variables[k.text[:j]]=k.text[j+1:]
 
+if 'yes' in variables["mailing_address"]:
+    variables.update({'mailing_street':variables['business_street']})
+    variables.update({'mailing_city':variables['business_city']})
+    variables.update({'mailing_zip_code':variables['business_zip_code']})
+print('\n')
+print(variables)
 if 'Inc' in variables["legal_company_name"]:
     register_corp.run(variables["legal_company_name"],variables["business_street"],variables["business_city"],variables["business_zip_code"])
     if 'San Bernardino' in variables['county']:
@@ -30,13 +39,13 @@ elif 'Llc' in variables["legal_company_name"]:
         fbn_san_bernardino.run(variables["legal_company_name"],variables["county"],variables["business_street"],variables["business_city"],variables["business_zip_code"],variables["business_state"],variables["phone_number"],variables["your_name"],variables["date_commenced"])
     else:
         fbn_la_.run(variables["legal_company_name"],variables["business_street"],variables["business_city"],variables["business_zip_code"],variables["organizer_name"])
-    infostatement_llc.run(entity_number,variables["business_street"],variables["business_city"],variables["business_zip_code"],owner_first_name,owner_last_name,type_of_business,variables["organizer_email"])
-    operating_agreement.run(owner_last_name,owner_first_name,wife_last_name,wife_name,company_name,aog_date,variables["business_street"],variables["business_city"],variables["business_zip_code"],variables["business_state"],type_of_business)
+    infostatement_llc.run(variables["entity_number"],variables["business_street"],variables["business_city"],variables["business_zip_code"],owner_first_name,owner_last_name,variables["type_of_business"],variables["organizer_email"])
+    operating_agreement.run(owner_last_name,owner_first_name,wife_last_name,wife_name,company_name,aog_date,variables["business_street"],variables["business_city"],variables["business_zip_code"],variables["business_state"],variables["type_of_business"])
 else:
     individual=True
     if 'San Bernardino' in variables["county"]:
-        fbn_san_bernardino.run(variables["legal_company_name"],variables["county"],variables["business_street"],variables["business_city"],variables["business_zip_code"],variables["business_state"],variables["phone_number"],your_name,date_commenced)
+        fbn_san_bernardino.run(variables["legal_company_name"],variables["county"],variables["business_street"],variables["business_city"],variables["business_zip_code"],variables["business_state"],variables["phone_number"],variables["your_name"],variables["date_commenced"])
     else:
-        fbn_la_.run(company_name,variables["business_street"],variables["business_city"],variables["business_zip_code"],variables["organizer_name"])
-    operating_agreement.run(owner_last_name,owner_first_name,wife_last_name,wife_name,company_name,aog_date,variables["business_street"],variables["business_city"],variables["business_zip_code"],variables["business_state"],type_of_business)
+        fbn_la_.run(variables["legal_company_name"],variables["business_street"],variables["business_city"],variables["business_zip_code"],variables["organizer_name"])
+    operating_agreement.run(owner_last_name,owner_first_name,wife_last_name,wife_name,variables["legal_company_name"],aog_date,variables["business_street"],variables["business_city"],variables["business_zip_code"],variables["business_state"],variables["type_of_business"])
     
